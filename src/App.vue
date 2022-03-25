1f1342bc51739b8ac63e8ba581b9e9ac560ea31d
Irtaza Mubarik <irtazamubarik@gmail.com>
2022-03-26 03:00:08 +0500

    toggled read-more

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo pariatur
+      necessitatibus cupiditate blanditiis architecto eum rerum ullam nulla,
+      culpa, adipisci quaerat similique sint.<span v-if="!more">....</span>
+      <span v-else>
+        Veniam corrupti harum asperiores! Omnis, error quam! Cum fuga esse aut
+        nesciunt? Ab eligendi minus ut, temporibus, cum, tempore provident at
+        iure harum eius placeat a iste odit quos ipsum nisi pariatur voluptates.
+        Vero veritatis odio cum? Eius, optio. Quo ex, quas deserunt aperiam
+        provident beatae laudantium nisi obcaecati et doloremque error eos vitae
+        autem itaque natus sapiente molestiae. Totam exercitationem officiis
+        mollitia rem eveniet ex cupiditate.
+      </span>
+    </p>
+    <button @click="toggleVisibility">
+      Read
+      <span v-show="more">Less</span>
+      <span v-show="!more">More</span>
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      more: false,
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      return (this.more = !this.more);
+    },
   },
 };
 </script>
@@ -22,5 +46,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: blueviolet;
+  color: white;
 }
 </style>
